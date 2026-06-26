a756aa7bf438729589acaa3dd85006f9b09861f0
Mark <mega.nemirovskiy@gmail.com>
2026-06-26 13:22:02 +0300

    сделал footer

--- client/src/components/layout/footer.tsx
+++ client/src/components/layout/footer.tsx
@@ -1,5 +1,15 @@
+import VK from '/src/assets/svg/VK.svg'
+import TG from '/src/assets/svg/TG.svg'
+import Insta from '/src/assets/svg/Instagram.svg'
+
+import '../../style/footer.scss'
+
 export default function Footer(){
     return (
-        <footer></footer>
+        <footer>
+            <a href="https://t.me/exchron0s"><img src={TG} alt="" /></a>
+            <a href="https://vk.com/exchron0s"><img src={VK} alt="" /></a>
+            <a href="https://www.instagram.com/exchron0s/"><img src={Insta} alt="" /></a>
+        </footer>
     )
 }
